b5417bae663395bf2880d1509668d601dfa6b0d0
allanvinicius <allanviniciosdemoura@gmail.com>
2024-01-23 17:18:45 -0300

    feat: create objects to emphasis and tendencies section

--- src/app/page.js
+++ src/app/page.js
@@ -7,6 +7,12 @@ import thumb03 from "@/assets/img-nft/03.jpg";
 import thumb04 from "@/assets/img-nft/04.jpg";
 import thumb05 from "@/assets/img-nft/05.jpg";
 import thumb06 from "@/assets/img-nft/06.jpg";
+import thumb07 from "@/assets/img-nft/07.jpg";
+import thumb08 from "@/assets/img-nft/08.jpg";
+import thumb09 from "@/assets/img-nft/09.jpg";
+import thumb10 from "@/assets/img-nft/10.jpg";
+import thumb11 from "@/assets/img-nft/11.jpg";
+import thumb12 from "@/assets/img-nft/12.jpg";
 
 const featuredCollections = [
   {
@@ -33,6 +39,9 @@ const featuredCollections = [
     price: "0.0721",
     price_brl: "602,02",
   },
+];
+
+const featuredEmphasis = [
   {
     name: "Cat #221",
     thumb: thumb05,
@@ -42,6 +51,45 @@ const featuredCollections = [
   {
     name: "Cat #221",
     thumb: thumb06,
+    price: "0.0721",
+    price_brl: "602,02",
+  },
+  {
+    name: "Cat #221",
+    thumb: thumb07,
+    price: "0.0721",
+    price_brl: "602,02",
+  },
+  {
+    name: "Cat #221",
+    thumb: thumb08,
+    price: "0.0721",
+    price_brl: "602,02",
+  },
+];
+
+const featuredTendencies = [
+  {
+    name: "Cat #221",
+    thumb: thumb09,
+    price: "0.0721",
+    price_brl: "602,02",
+  },
+  {
+    name: "Cat #221",
+    thumb: thumb10,
+    price: "0.0721",
+    price_brl: "602,02",
+  },
+  {
+    name: "Cat #221",
+    thumb: thumb11,
+    price: "0.0721",
+    price_brl: "602,02",
+  },
+  {
+    name: "Cat #221",
+    thumb: thumb12,
     price: "0.0721",
     price_brl: "602,02",
   },
@@ -61,13 +109,13 @@ export default function Home() {
       <SectionCarrousel
         title="Destaque do Mês"
         subtitle="Seleção Mensal"
-        data={featuredCollections}
+        data={featuredEmphasis}
       />
 
       <SectionCarrousel
         title="Tendências"
         subtitle="Em Tendência"
-        data={featuredCollections}
+        data={featuredTendencies}
       />
     </>
   );
