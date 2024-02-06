c78820068f7fa1495c345b1c5c742e31ed0de9e4
allanvinicius <allanviniciosdemoura@gmail.com>
2024-02-06 17:45:48 -0300

    feat: add gsap in banner

--- src/components/section-banner/index.js
+++ src/components/section-banner/index.js
@@ -1,13 +1,45 @@
+"use client";
+
+import { useEffect, useRef } from "react";
 import Link from "next/link";
 import Image from "next/image";
+
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
 import ContainerGrid from "../container";
 
 import playStore from "@/assets/google.svg";
 import appleStore from "@/assets/apple.svg";
 
 export default function SectionBanner() {
+  const bannerRef = useRef(null);
+
+  useEffect(() => {
+    const banner = bannerRef.current;
+
+    gsap.fromTo(
+      banner,
+      {
+        opacity: 0,
+        scale: 0.7,
+      },
+      {
+        opacity: 1,
+        scale: 1,
+        ease: "power3.out",
+        duration: 1.5,
+        scrollTrigger: {
+          trigger: banner,
+          start: "top-=100% center",
+        },
+      }
+    );
+  }, []);
+
   return (
-    <section className="py-10 @desktop:py-28">
+    <section className="py-10 @desktop:py-28" ref={bannerRef}>
       <ContainerGrid>
         <div className="bg-bannerBg bg-no-repeat bg-cover rounded-2xl overflow-hidden px-3 py-10 @desktop:px-28 @desktop:py-20 flex flex-col items-center">
           <h2 className="font-poppins font-bold text-titleBannerMobile @desktop:text-titleBanner/normal text-center max-w-titleBanner">
